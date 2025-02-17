ffc6393c00e86dc4779ec876fb56d74c46f89800
evandrosystems <evandrosystems@gmail.com>
2025-02-17 04:12:31 -0300

    refactor: create withdraw function

--- index.py
+++ index.py
@@ -17,6 +17,31 @@ def depositar(saldo, valor, extrato, /):
 
     return saldo, extrato
 
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\nVocê não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("\nO valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("\nNúmero máximo de saques excedido.")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+
+    else:
+        print("\nO valor informado é inválido.")
+
+    return saldo, extrato
+
 def main():
     saldo = 0
     limite = 500
@@ -34,29 +59,14 @@ def main():
 
         elif opcao == "s":
             valor = float(input("Informe o valor do saque: "))
-
-            excedeu_saldo = valor > saldo
-
-            excedeu_limite = valor > limite
-
-            excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-            if excedeu_saldo:
-                print("Você não tem saldo suficiente.")
-
-            elif excedeu_limite:
-                print("O valor do saque excede o limite.")
-
-            elif excedeu_saques:
-                print("Número máximo de saques excedido.")
-
-            elif valor > 0:
-                saldo -= valor
-                extrato += f"Saque: R$ {valor:.2f}\n"
-                numero_saques += 1
-
-            else:
-                print("O valor informado é inválido.")
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES
+            )
 
         elif opcao == "e":
             print("\n================ EXTRATO ================")
