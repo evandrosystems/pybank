fb03ca0ec6ca8c1744633520f2b2b156d9bdd6fa
evandrosystems <evandrosystems@gmail.com>
2025-03-01 14:12:37 -0300

    feat: create person class

--- index.py
+++ index.py
@@ -12,7 +12,15 @@ class Cliente:
 
     def adicionar_conta(self, conta):
         self.contas.append(conta)
-        
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
 class Conta:
     def __init__(self, numero, cliente):
         self.saldo = 0,
