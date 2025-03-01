25c9646209680b8f1439d7d43fdb71e3116b058b
evandrosystems <evandrosystems@gmail.com>
2025-03-01 14:15:27 -0300

    feat: create withdrawal class

--- index.py
+++ index.py
@@ -137,7 +137,21 @@ class Transacao(ABC):
     @abstractclassmethod
     def registrar(self, conta):
         pass
-    
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+            
 def menu():
     menu = """
     [d] Depositar
