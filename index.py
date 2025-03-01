ab48466ce42c80af2bdfacf260730ef3f922f7ed
evandrosystems <evandrosystems@gmail.com>
2025-03-01 14:30:18 -0300

    refactor: show extract function

--- index.py
+++ index.py
@@ -242,10 +242,30 @@ def sacar(clientes):
 
     cliente.realizar_transacao(conta, transacao)
 
-def exibir_extrato(saldo, /, *, extrato):
+def exibir_extrato(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+
+    if not cliente:
+        print("\n@@@ Cliente não encontrado! @@@")
+        return
+
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+
     print("\n================ EXTRATO ================")
-    print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    transacoes = conta.historico.transacoes
+
+    extrato = ""
+    if not transacoes:
+        extrato = "Não foram realizadas movimentações."
+    else:
+        for transacao in transacoes:
+            extrato += f"\n{transacao['tipo']}:\n\tR$ {transacao['valor']:.2f}"
+
+    print(extrato)
+    print(f"\nSaldo:\n\tR$ {conta.saldo:.2f}")
     print("==========================================")
 
 def criar_usuario(usuarios):
