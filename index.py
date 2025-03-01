4bb77a0a5875d5de6e2ba5e0e2e58df4978379f8
evandrosystems <evandrosystems@gmail.com>
2025-03-01 14:16:06 -0300

    feat: create deposit class

--- index.py
+++ index.py
@@ -151,7 +151,21 @@ class Saque(Transacao):
 
         if sucesso_transacao:
             conta.historico.adicionar_transacao(self)
-            
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+                  
 def menu():
     menu = """
     [d] Depositar
